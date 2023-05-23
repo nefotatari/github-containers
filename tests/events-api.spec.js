d62befa14988225ed0a60ad43f5011b1af7c4979
nefotatari <51050971+nefotatari@users.noreply.github.com>
2023-05-23 11:06:14 +0200

    Update events-api.spec.js

--- tests/events-api.spec.js
+++ tests/events-api.spec.js
@@ -21,5 +21,5 @@ test('getting events', async ({ request }) => {
   const resDataRaw = await response.body();
   const resData = JSON.parse(resDataRaw.toString());
   expect(resData).toHaveProperty('events');
-  expect(resData.events.length).toBeGreaterThan(0);
+  expect(resData.events.length).toBeGreaterThan(3);
 });
